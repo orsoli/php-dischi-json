31d42cf2fb0b7c7e87b1daadc2d0cdde9540ced2
orsoli <fljcoli@gmail.com>
2024-10-08 20:10:46 +0200

    Finish Bonus

--- app/js/main.js
+++ app/js/main.js
@@ -4,9 +4,12 @@ createApp({
   data() {
     return {
       songsList: [], // Store the song lists
+      clickedSong: {}, // Store clicked song cards
+      isActive: false, // Flag using to toggle class d-none in modal html
     };
   },
   methods: {
+    // Define a function to get api
     getSongList() {
       axios
         .get("http://localhost:8888/php-dischi-json/db/dischi.json")
@@ -19,6 +22,17 @@ createApp({
           // handle error
           console.log(error);
         });
+    },
+    // Define a function to get cliicked song
+    getClickedSong(i) {
+      this.clickedSong = this.songsList[i];
+      console.log(this.clickedSong); // test console log
+      this.isActive = true;
+      console.log(this.isActive); // Test console log
+    },
+    //Define function to close the modal
+    closeModal() {
+      this.isActive = false;
     },
   },
 
